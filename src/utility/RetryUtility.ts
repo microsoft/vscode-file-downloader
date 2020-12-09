04c796c57ea2a2ad4c87cca767b413272f3b19aa
Rakesh Reddy Vanga <rareddyv@microsoft.com>
2020-12-08 16:36:41 -0800

    Add retries in file downloader

---
+++ src/utility/RetryUtility.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export class RetryUtility {
+    public static async exponentialRetryAsync<T>(requestFn: () => Promise<T>, retries: number, delayInMs: number): Promise<T> {
+        try {
+            return await requestFn();
+        }
+        catch (error) {
+            if (retries === 0) {
+                throw error;
+            }
+            else {
+                await new Promise((resolve): void => {
+                  setTimeout(resolve, delayInMs);
+                });
+                return this.exponentialRetryAsync(requestFn, retries - 1, delayInMs * 2);
+            }
+        }
+    }
+}
